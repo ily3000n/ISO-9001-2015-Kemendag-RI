b38b7a43049129f9cad71a457aa41a8571ef7924
Muhammad <muhammadfabil40@gmail.com>
2024-07-11 11:26:58 +0700

    mobile

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -5,36 +5,36 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Header = () => {
   return (
-    <div className="h-full w-full bg-white flex items-center justify-between overflow-hidden p-4">
+    <div className="h-full w-full bg-white flex items-center justify-center p-3">
       <div className="flex items-center w-full justify-between">
-        <div className="w-2/4 flex justify-start ">
+        <div className="flex items-center justify-start w-2/4">
           <Image 
             src="/logo.svg" // Replace with the path to your logo image
             alt="Logo"
-            width={250} // Adjust width as needed
-            height={250} // Adjust height as needed
+            width={200} // Adjust width as needed
+            height={200} // Adjust height as needed
             className="object-contain"
           />
         </div>
-        <div className="w-2/4 flex justify-end">
-          <h1 className="md:text-7xl text-2xl lg:text-xl font-bold text-blue-950 text-left">
-          <TypeAnimation
-            sequence={[
-             
-              'ISO',
-              1000,
-              'ISO 9001 ',
-              1000,
-              'ISO 9001 : ',
-              1000,
-              'ISO 9001 : 2015',
-              1000,
-            ]}
-            speed={50}
-            style={{ fontSize: '2rem' }}
-            repeat={Infinity}
-          />
-          </h1>
+        <div className="flex items-center justify-end w-2/4">
+          {/* <h1 className="md:text-7xl text-2xl lg:text-md font-bold text-blue-950 text-left"> */}
+            <TypeAnimation
+              sequence={[
+                'ISO',
+                1000,
+                'ISO 9001 ',
+                1000,
+                'ISO 9001 : ',
+                1000,
+                'ISO 9001 : 2015',
+                1000,
+              ]}
+              speed={50}
+              style={{ fontSize: '2rem' }}
+              repeat={Infinity}
+              className="md:text-7xl text-2xl lg:text-md font-bold text-blue-950 text-left"
+            />
+          {/* </h1> */}
         </div>
       </div>
     </div>
